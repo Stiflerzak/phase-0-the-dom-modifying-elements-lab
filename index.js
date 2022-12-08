9933039089e84d690b809d640c72099338d6b0b8
Stiflerzak <hilarymwendia97@gmail.com>
2022-12-08 17:48:21 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main= document.getElementById('main')
+const jina= "Hilary"
+main.remove()
+
+const newHeader= document.createElement('h1')
+document.body.append(newHeader);
+
+
+newHeader.id='victory';
+
+newHeader.innerHTML= `${jina} is the champion`
+
+
+
